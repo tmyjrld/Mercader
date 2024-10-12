2c8321e8881cb3a61c5620ee9bf21643713a2d8c
tmyjrld <timmy.mercader3@gmail.com>
2024-10-12 17:33:00 +0800

    home page address changed

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePageView, AboutPageView, ContactPageView
 
 urlpatterns = [
-    path('home/', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('contacts/', ContactPageView.as_view(), name='contact'),
 ]
