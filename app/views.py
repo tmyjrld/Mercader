a0e6fe688ad242cbe2d0c0c849419a916259d764
tmyjrld <timmy.mercader3@gmail.com>
2024-10-12 16:14:49 +0800

    commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'app/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'app/About.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'app/contact.html'
+
